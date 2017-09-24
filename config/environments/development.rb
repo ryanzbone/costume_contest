0d38eb60adcfa4ea55cb90f7249f2d910114b3b6
Ryan Bone <rbone@covermymeds.com>
2017-09-24 13:04:37 -0400

    update config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,13 +52,13 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.paperclip_defaults = {
-    #storage: :s3,
-    #s3_credentials: {
-      #bucket: ENV.fetch('S3_BUCKET_NAME'),
-      #access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      #secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      #s3_region: ENV.fetch('AWS_REGION'),
-    #}
+  Paperclip::Attachment.default_options[:s3_credentials] = {
+    bucket: ENV.fetch('DEV_S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
+  Paperclip::Attachment.default_options[:s3_region] = 'us-east-2'
+  Paperclip::Attachment.default_options[:s3_host_name] = 's3.us-east-2.amazonaws.com'
+  Paperclip.options[:command_path] = 'usr/local/bin'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,13 +89,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
-    }
+  Paperclip::Attachment.default_options[:s3_credentials] = {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
+  Paperclip::Attachment.default_options[:s3_region] = 'us-east-2'
+  Paperclip::Attachment.default_options[:s3_host_name] = 's3.us-east-2.amazonaws.com'
+  Paperclip.options[:command_path] = 'usr/local/bin'
 end
